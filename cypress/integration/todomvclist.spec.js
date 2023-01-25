ed1dc7375a344af8abb6f0dd861b57700784d16b
Geovanni <roderik317@gmail.com>
2023-01-25 11:59:55 -0600

    adding new command for interacte with the practice webpage

--- cypress/integration/todomvclist.spec.js
+++ cypress/integration/todomvclist.spec.js
@@ -2,8 +2,8 @@
 
 describe('suite of test of cypress in todomvc page', function(){
 
-    it('go to todomvc page', () => {
+    it('should be able to add a new to do element', () => {
         cy.visit('https://todomvc-app-for-testing.surge.sh/')
-
+        cy.get('.new-todo').type("Create a awesome thing{enter}")
     })
 });
