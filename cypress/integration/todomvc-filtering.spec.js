3b46ed4c3513e0ee895db72848c3bece508d8399
Geovanni <roderik317@gmail.com>
2023-01-26 01:46:56 -0600

    valideting todo elements after filtering

--- cypress/integration/todomvc-filtering.spec.js
+++ cypress/integration/todomvc-filtering.spec.js
@@ -13,5 +13,19 @@ describe('filtering todo elements', () =>{
 
     it('flitering to "Active" todo elements', () => {
         cy.contains("Active").click()
+        cy.get('.todo-list li').should('have.length.lessThan', 3)
+        cy.get('.todo-list li').should('not.have.length.greaterThan', 5)
+    })
+
+    it('flitering to "Completed" todo elements', () => {
+        cy.contains("Completed").click()
+        cy.get('.todo-list li').should('have.length.lessThan', 3)
+        cy.get('.todo-list li').should('not.have.length.greaterThan', 3)
+    })
+
+    it('flitering to "All" todo elements', () => {
+        cy.contains("All").click()
+        cy.get('.todo-list li').should('have.length.lessThan', 5)
+        cy.get('.todo-list li').should('have.length', 3)
     })
 })
